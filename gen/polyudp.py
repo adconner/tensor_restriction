bc3a9cb40ef0745b309302dc8731487a6e6f7bff
Austin Conner <aconner.vu@gmail.com>
2017-06-10 00:47:46 -0500

    python support working, required a suspicious inefficency

--- gen/polyudp.py
+++ gen/polyudp.py
@@ -1,34 +1,34 @@
 import ctypes
 import numpy as np
 import pygmo
+from numpy.ctypeslib import ndpointer
 
 class PolyUDP:
     def __init__(self):
-        from numpy.ctypeslib import ndpointer
         lib = ctypes.cdll.LoadLibrary('./lib.so')
         self.xs = int(lib.xs())
-        self.f = lib.f
-        self.f.restype = ctypes.c_double
-        self.f.argtypes = [ndpointer(ctypes.c_double,shape=(self.xs,),flags='C_CONTIGUOUS')]
-        self.jac = lib.jac
-        self.jac.argtypes = [ndpointer(ctypes.c_double,shape=(self.xs,),flags='C_CONTIGUOUS'),
-                ndpointer(ctypes.c_double,shape=(self.xs,),flags='C_CONTIGUOUS')]
-        self.hess = lib.hess
-        self.hess.argtypes = [ndpointer(ctypes.c_double,shape=(self.xs,),flags='C_CONTIGUOUS'),
-                ndpointer(ctypes.c_double,shape=(self.xs*(self.xs+1)/2,),flags='C_CONTIGUOUS')]
 
     def fitness(self,x):
-        return self.f(x)
-
-    def get_bounds(self,):
-        return (-2*np.ones(self.xs),2*np.ones(self.xs))
+        f = ctypes.cdll.LoadLibrary('./lib.so').f
+        f.restype = ctypes.c_double
+        f.argtypes = [ndpointer(ctypes.c_double,shape=(self.xs,),flags='C_CONTIGUOUS')]
+        return (f(x),)
 
     def gradient(self,x):
+        jac = ctypes.cdll.LoadLibrary('./lib.so').jac
+        jac.argtypes = [ndpointer(ctypes.c_double,shape=(self.xs,),flags='C_CONTIGUOUS'),
+                ndpointer(ctypes.c_double,shape=(self.xs,),flags='C_CONTIGUOUS')]
         gr = np.zeros(self.xs)
-        self.jac(x,gr)
+        jac(x,gr)
         return gr
 
     def hessians(self,x):
+        hess = ctypes.cdll.LoadLibrary('./lib.so').hess
+        hess.argtypes = [ndpointer(ctypes.c_double,shape=(self.xs,),flags='C_CONTIGUOUS'),
+                ndpointer(ctypes.c_double,shape=(self.xs*(self.xs+1)/2,),flags='C_CONTIGUOUS')]
         he = np.zeros(self.xs * (self.xs+1)/2)
-        self.hess(x,he)
+        hess(x,he)
         return he
+
+    def get_bounds(self,):
+        return (-2*np.ones(self.xs),2*np.ones(self.xs))
