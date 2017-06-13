1a81079f8f0fe8159b4182493dc4fc0411fc1d26
Austin Conner <aconner.vu@gmail.com>
2017-06-13 17:11:05 -0500

    playing around with options

--- gen/main.cpp
+++ gen/main.cpp
@@ -24,17 +24,33 @@ int main(int argc, char** argv) {
   Solver::Options options;
   options.minimizer_progress_to_stdout = true;
   /* options.max_num_iterations = 10000; */
+  /* options.max_num_iterations = 1000; */
   options.num_threads = 4;
   options.num_linear_solver_threads = 4;
 
-  /* options.minimizer_type = TRUST_REGION; */
+  /* options.minimizer_type = LINE_SEARCH; */
   // trust region options
-  /* options.use_inner_iterations = true; */
+  options.trust_region_strategy_type = LEVENBERG_MARQUARDT;
   options.use_nonmonotonic_steps = true;
-  /* options.trust_region_strategy_type = // */
+  /* options.use_inner_iterations = true; */
+
+  // line search options
+  /* options.line_search_direction_type = NONLINEAR_CONJUGATE_GRADIENT; */
+  /* options.line_search_type = ARMIJO; */
+  /* options.nonlinear_conjugate_gradient_type = POLAK_RIBIERE; */
+  /* options.nonlinear_conjugate_gradient_type = HESTENES_STIEFEL; */
 
   // linear solver options
-  /* options.dynamic_sparsity = true; // since solutions are typically sparse? */
+  options.linear_solver_type = SPARSE_NORMAL_CHOLESKY; // since solutions are typically sparse?
+  options.sparse_linear_algebra_library_type = SUITE_SPARSE; // since solutions are typically sparse?
+  options.dynamic_sparsity = true; // since solutions are typically sparse?
+  /* options.use_postordering = true; */
+
+  /* options.linear_solver_type = DENSE_QR; // since solutions are typically sparse? */
+  /* options.dense_linear_algebra_library_type = LAPACK; // since solutions are typically sparse? */
+
+  /* options.linear_solver_type = CGNR; */
+  /* options.linear_solver_type = ITERATIVE_SCHUR; */
 
   Solver::Summary summary;
   Solve(options, &problem, &summary);
