daae39bc1cf15bd4c23de2b9ac109ef100299f28
Austin Conner <aconner.vu@gmail.com>
2017-06-13 16:29:41 -0500

    print out full precision answer

--- gen/main.cpp
+++ gen/main.cpp
@@ -1,6 +1,7 @@
 #include <random>
 #include <fstream>
 #include <iterator>
+#include <limits>
 #include "ceres/ceres.h"
 #include "glog/logging.h"
 #include "prob.h"
@@ -22,11 +23,25 @@ int main(int argc, char** argv) {
 
   Solver::Options options;
   options.minimizer_progress_to_stdout = true;
+  /* options.max_num_iterations = 10000; */
+  options.num_threads = 4;
+  options.num_linear_solver_threads = 4;
+
+  /* options.minimizer_type = TRUST_REGION; */
+  // trust region options
+  /* options.use_inner_iterations = true; */
+  options.use_nonmonotonic_steps = true;
+  /* options.trust_region_strategy_type = // */
+
+  // linear solver options
+  /* options.dynamic_sparsity = true; // since solutions are typically sparse? */
+
   Solver::Summary summary;
   Solve(options, &problem, &summary);
 
   cout << summary.FullReport() << "\n";
   ofstream out("out.txt");
+  out.precision(numeric_limits<double>::max_digits10);
   copy(x,x+N,ostream_iterator<double>(out,"\n"));
   return 0;
 }
