192dc91fb7fcc5d305f31fa9eba19bdf7ea5841d
Austin Conner <aconner.vu@gmail.com>
2017-06-14 16:41:36 -0500

    tweak solver options

--- gen/main.cpp
+++ gen/main.cpp
@@ -72,26 +72,26 @@ void solver_opts(Solver::Options &options) {
   /* options.minimizer_type = LINE_SEARCH; */
   // trust region options
   options.trust_region_strategy_type = LEVENBERG_MARQUARDT;
-  options.use_nonmonotonic_steps = true;
-  /* options.use_inner_iterations = true; */
+  /* options.use_nonmonotonic_steps = true; */
+  options.use_inner_iterations = true;
 
   // line search options
-  /* options.line_search_direction_type = NONLINEAR_CONJUGATE_GRADIENT; */
+  options.line_search_direction_type = BFGS;
   /* options.line_search_type = ARMIJO; */
   /* options.nonlinear_conjugate_gradient_type = POLAK_RIBIERE; */
   /* options.nonlinear_conjugate_gradient_type = HESTENES_STIEFEL; */
 
   // linear solver options
   options.linear_solver_type = SPARSE_NORMAL_CHOLESKY;
-  options.sparse_linear_algebra_library_type = SUITE_SPARSE;
   options.dynamic_sparsity = true; // since solutions are typically sparse?
   /* options.use_postordering = true; */
 
   /* options.linear_solver_type = DENSE_QR; */
-  /* options.dense_linear_algebra_library_type = LAPACK; */
-
   /* options.linear_solver_type = CGNR; */
   /* options.linear_solver_type = ITERATIVE_SCHUR; */
+
+  options.sparse_linear_algebra_library_type = SUITE_SPARSE;
+  options.dense_linear_algebra_library_type = LAPACK;
 }
 
 enum DiscreteMode {
