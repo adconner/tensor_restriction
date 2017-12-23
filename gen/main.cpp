71f44b25aa89136e4501ba485024bdfc5dcff12f
Austin Conner <aconner.vu@gmail.com>
2017-12-22 22:37:23 -0600

    tune for rank solutions

--- gen/main.cpp
+++ gen/main.cpp
@@ -29,14 +29,14 @@ const int l2_reg_steps = 3;
 const double l2_reg_decay = 0.60;
 const double alphastart = 0.01;
 const double ftol_rough = 1e-3;
-const double abort_worse = 1e-3;
+const double abort_worse = 1e-20;
 
 const double solved_fine = 1e-25;
 const double attempt_sparse_thresh = 1e-12;
 
 // parameters for finding an initial solution
 const int iterations_rough = 200;
-const int iterations_fine = 2500;
+const int iterations_fine = 500;
 
 // parameters for descretization
 const int iterations_discrete = 60;
@@ -448,7 +448,9 @@ int main(int argc, char** argv) {
       greedy_discrete(problem,x,options,eopts,successes,DA_ZERO,N/10+1);
       greedy_discrete(problem,x,options,eopts,successes,DA_PM_ONE_ZERO,N/10+1);
     }
-    /* greedy_discrete_pairs(problem,x,options,eopts,10); */
+    /* greedy_discrete_pairs(problem,x,options,eopts,N/10+1); */
+    /* greedy_discrete(problem,x,options,eopts,successes,DA_ZERO,N/10+1); */
+    /* greedy_discrete(problem,x,options,eopts,successes,DA_PM_ONE_ZERO,N/10+1); */
 
     logsol(x,"out.txt");
   }
