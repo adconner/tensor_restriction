96db4801b944ad39c612c37a3e1d8386850f213a
Austin Conner <aconner.vu@gmail.com>
2017-12-26 01:20:35 -0600

    tweak parameters

--- gen/opts.h
+++ gen/opts.h
@@ -19,14 +19,13 @@ const int l2_reg_steps = 3;
 const double l2_reg_decay = 0.60;
 const double alphastart = 0.01;
 const double ftol_rough = 1e-3;
-const double abort_worse = 1e-20;
+const int iterations_rough = 500;
+const double abort_worse = 1e-4;
+
+const int iterations_fine = 1000;
 
 const double solved_fine = 1e-25;
 const double attempt_sparse_thresh = 1e-12;
-
-// parameters for finding an initial solution
-const int iterations_rough = 200;
-const int iterations_fine = 500;
 
 // parameters for descretization
 const int iterations_discrete = 60;
