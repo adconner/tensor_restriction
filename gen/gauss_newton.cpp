fa89f2b6a65680701401174beebdc99dd82d8137
Austin Conner <aconner.vu@gmail.com>
2020-07-26 00:45:28 -0500

    dont do line search, seems not useful for my purpose

--- gen/gauss_newton.cpp
+++ gen/gauss_newton.cpp
@@ -2,7 +2,7 @@
 #include "gauss_newton.h"
 /* #define SVD_GAUSS_NEWTON */
 
-void gauss_newton(Problem &p, double *x, double ftol, int max_it, double rcond) {
+void gauss_newton(Problem &p, double *x, double xtol, int max_it, double rcond) {
   double cost; 
   vector<double> rs;
   CRSMatrix jac_sparse; 
@@ -24,7 +24,7 @@ void gauss_newton(Problem &p, double *x, double ftol, int max_it, double rcond) 
 #endif
 
   vector<double> sav(MULT*N);
-  for (int it = 1; ; ++it) {
+  for (int it = 1; it <= max_it ; ++it) {
     jac.assign(jac.size(),0.0);
     for (int i=0; i < m; ++i) {
       for (int j=jac_sparse.rows[i]; j < jac_sparse.rows[i+1]; ++j) {
@@ -45,26 +45,27 @@ void gauss_newton(Problem &p, double *x, double ftol, int max_it, double rcond) 
         jpvt.data(), rcond, &rank);
 #endif
 
-    auto f = [&](double t) {
-      copy(x,x+MULT*N,sav.begin());
-      for (int i=0; i<n; ++i) {
-        x[xis[i/MULT]*MULT+i%MULT] -= t*rs[i];
-      }
-      double cost; p.Evaluate(Problem::EvaluateOptions(),&cost,0,0,0);
-      copy(sav.begin(),sav.end(),x);
-      return cost;
-    };
+    /* auto f = [&](double t) { */
+    /*   copy(x,x+MULT*N,sav.begin()); */
+    /*   for (int i=0; i<n; ++i) { */
+    /*     x[xis[i/MULT]*MULT+i%MULT] -= t*rs[i]; */
+    /*   } */
+    /*   double cost; p.Evaluate(Problem::EvaluateOptions(),&cost,0,0,0); */
+    /*   copy(sav.begin(),sav.end(),x); */
+    /*   return cost; */
+    /* }; */
 
-    double lo=0.0, hi=1.0;
-    while (hi-lo > 1e-10) {
-      double lmid = (2*lo+hi) / 3, hmid = (lo+2*hi) / 3;
-      if (f(lmid) < f(hmid)) {
-        hi = hmid;
-      } else {
-        lo = lmid;
-      }
-    }
-    double t0 = (lo + hi) / 2;
+    /* double lo=0.0, hi=10.0; */
+    /* while (hi-lo > 1e-10) { */
+    /*   double lmid = (2*lo+hi) / 3, hmid = (lo+2*hi) / 3; */
+    /*   if (f(lmid) < f(hmid)) { */
+    /*     hi = hmid; */
+    /*   } else { */
+    /*     lo = lmid; */
+    /*   } */
+    /* } */
+    /* double t0 = (lo + hi) / 2; */
+    double t0 = 1.0;
 
     double dx = 0.0;
     for (int i=0; i<n; ++i) {
@@ -77,8 +78,10 @@ void gauss_newton(Problem &p, double *x, double ftol, int max_it, double rcond) 
     if (verbose) {
       cout << it << " " << acost << " " << m << "x" << n << " jac, rank " << rank << " dx " << std::sqrt(dx) << " " << t0 << endl;
     }
-    /* if ( (cost-acost) / cost < ftol ) */
-    /*   break; */
+    if (acost < 1e-26)
+      break;
+    if (dx < xtol)
+      break;
     cost = acost;
   }
 
--- gen/gauss_newton.h
+++ gen/gauss_newton.h
@@ -2,6 +2,6 @@
 #define _GAUSS_NEWTON_H_
 #include "util.h"
 
-void gauss_newton(Problem &p, double *x, double ftol, int max_it, double rcond=1e-14); 
+void gauss_newton(Problem &p, double *x, double xtol, int max_it, double rcond=1e-14); 
 
 #endif
