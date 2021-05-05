f861c1bca049aded665c7b6d745d8a43d7876af0
Austin Conner <aconner.vu@gmail.com>
2021-05-04 22:25:21 -0400

    als implementation for restriction

---
+++ gen/restrict/als.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <cstring>
+
+#include <lapacke.h>
+#include <cblas.h>
+
+#include "tensor.h"
+
+using namespace std;
+
+void als(double *x, int group) {
+  // let ta in {TA,TB,TC} correspond to group
+  // and let tb tc be the other two (in cyclic order after ta)
+
+  // we least squares solve AX = B, A is tc tb x sa, X is sa x ta , B is tc tb x ta
+  // everything col major
+
+  auto s = [=](int p, int a, int b, int c) {
+    switch ((group+p) % 3) {
+      case 0: return a;
+      case 1: return b;
+    }
+    return c;
+  };
+
+  double B[TA*TB*TC]; // considered as (TY*TZ) x TX matrix in col major order
+  for (int i=0; i<TA; ++i) {
+    for (int j=0; j<TB; ++j) {
+      for (int k=0; k<TC; ++k) {
+        B[(s(0,i,j,k)*s(1,TA,TB,TC) + s(1,i,j,k))*s(2,TA,TB,TC) + s(2,i,j,k)] = T[i][j][k];
+      }
+    }
+  }
+  double A[max(max(TA*TB*SC,TA*SB*TC),SA*TB*TC)];
+  memset(A,0,s(0,SA,SB,SC)*s(1,TA,TB,TC)*s(2,TA,TB,TC)*sizeof(double));
+  for (int t=0; t<SNZ; ++t) {
+    for (int j=0; j<s(1,TA,TB,TC); ++j) {
+      for (int k=0; k<s(2,TA,TB,TC); ++k) {
+        double cur = 0;
+        switch (group) {
+          case 0: cur = x[TA*SA+j*SB+SJ[t]] * x[TA*SA+TB*SB+k*SC+SK[t]]; break;
+          case 1: cur = x[TA*SA+TB*SB+j*SC+SK[t]] * x[k*SA+SI[t]]; break;
+          case 2: cur = x[j*SA+SI[t]] * x[TA*SA+k*SB+SJ[t]]; break;
+        }
+        A[(s(0,SI[t],SJ[t],SK[t])*s(1,TA,TB,TC)+j)*s(2,TA,TB,TC)+k] += cur;
+      }
+    }
+  }
+  int m = s(2,TA,TB,TC)*s(1,TA,TB,TC);
+  int n = s(0,SA,SB,SC);
+  int nrhs = s(0,TA,TB,TC);
+  LAPACKE_dgels(LAPACK_COL_MAJOR,'N',m,n,nrhs,A,m,B,m);
+  LAPACKE_dlacpy(LAPACK_COL_MAJOR,'N',n,nrhs,B,m,x+s(0,0,TA*SA,TA*SA+TB*SB),s(0,SA,SB,SC));
+}
---
+++ gen/restrict/als.h
@@ -0,0 +1,6 @@
+#ifndef _ALS_H_
+#define _ALS_H_
+
+void als(double *x, int group);
+
+#endif
