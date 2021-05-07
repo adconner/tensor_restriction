8f54fc3a41812cfd96f9297cbc35ad9f8850cce1
Austin Conner <aconner.vu@gmail.com>
2021-05-06 23:58:12 -0400

    fix bug!

--- gen/restrict/restrict.cpp
+++ gen/restrict/restrict.cpp
@@ -12,19 +12,27 @@ struct Equation : public Eq {
     set_num_residuals(MULT);
   }
   bool Evaluate(double const* const* x, double* residuals, double** jacobians) const {
+    if (jacobians) {
+      if (jacobians[0])
+        fill(jacobians[0],jacobians[0]+MULT*SA,0.0);
+      if (jacobians[1])
+        fill(jacobians[1],jacobians[1]+MULT*SB,0.0);
+      if (jacobians[2])
+        fill(jacobians[2],jacobians[2]+MULT*SC,0.0);
+    }
 #ifndef CX
     residuals[0] = -T[i][j][k];
     for (int t=0; t<SNZ; ++t) {
       residuals[0] += alpha*x[0][SI[t]]*x[1][SJ[t]]*x[2][SK[t]]*SV[t];
       if (jacobians) {
         if (jacobians[0]) {
-          jacobians[0][SI[t]] = alpha*x[1][SJ[t]]*x[2][SK[t]]*SV[t];
+          jacobians[0][SI[t]] += alpha*x[1][SJ[t]]*x[2][SK[t]]*SV[t];
         }
         if (jacobians[1]) {
-          jacobians[1][SJ[t]] = alpha*x[0][SI[t]]*x[2][SK[t]]*SV[t];
+          jacobians[1][SJ[t]] += alpha*x[0][SI[t]]*x[2][SK[t]]*SV[t];
         }
         if (jacobians[2]) {
-          jacobians[2][SK[t]] = alpha*x[0][SI[t]]*x[1][SJ[t]]*SV[t];
+          jacobians[2][SK[t]] += alpha*x[0][SI[t]]*x[1][SJ[t]]*SV[t];
         }
       }
     }
@@ -39,18 +47,18 @@ struct Equation : public Eq {
       if (jacobians) {
         if (jacobians[0]) {
           cx e = alpha*b*c*SV[t];
-          cx(jacobians[0][2*SI[t]],jacobians[0][2*SA+2*t]) = e;
-          cx(jacobians[0][2*SI[t]+1],jacobians[0][2*SA+2*t+1]) = cx(0,1)*e;
+          cx(jacobians[0][2*SI[t]],jacobians[0][2*SA+2*t]) += e;
+          cx(jacobians[0][2*SI[t]+1],jacobians[0][2*SA+2*t+1]) += cx(0,1)*e;
         }
         if (jacobians[1]) {
           cx e = alpha*a*c*SV[t];
-          cx(jacobians[1][2*SJ[t]],jacobians[1][2*SB+2*t]) = e;
-          cx(jacobians[1][2*SJ[t]+1],jacobians[1][2*SB+2*t+1]) = cx(0,1)*e;
+          cx(jacobians[1][2*SJ[t]],jacobians[1][2*SB+2*t]) += e;
+          cx(jacobians[1][2*SJ[t]+1],jacobians[1][2*SB+2*t+1]) += cx(0,1)*e;
         }
         if (jacobians[2]) {
           cx e = alpha*a*b*SV[t];
-          cx(jacobians[2][2*SK[t]],jacobians[2][2*SC+2*t]) = e;
-          cx(jacobians[2][2*SK[t]+1],jacobians[2][2*SC+2*t+1]) = cx(0,1)*e;
+          cx(jacobians[2][2*SK[t]],jacobians[2][2*SC+2*t]) += e;
+          cx(jacobians[2][2*SK[t]+1],jacobians[2][2*SC+2*t+1]) += cx(0,1)*e;
         }
       }
     }
